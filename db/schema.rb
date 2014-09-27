d9f9b3f8e3f536d8141a1f29b717339d07b531f0
Malgorzata Ksionek <meksionek@gmail.com>
2014-09-27 16:29:29 +0200

    add code

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927134220) do
+ActiveRecord::Schema.define(version: 20140927142800) do
 
   create_table "users", force: true do |t|
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "code"
   end
 
 end
