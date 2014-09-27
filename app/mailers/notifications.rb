f33c1978c9b1b0b38054b3ae72bac5c9512b25a7
Malgorzata Ksionek <meksionek@gmail.com>
2014-09-27 14:32:20 +0200

    add sendgrid options

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,5 +1,5 @@
 class Notifications < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "from@example.com" #od kogo - i tu można się podszywać ;P
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
